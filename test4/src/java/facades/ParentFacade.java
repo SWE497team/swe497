0a9830591aa34589e9047925e2cc3ea1b74d83c2
SWE497team <36336916+SWE497team@users.noreply.github.com>
2018-02-21 21:23:30 +0300

    Update ParentFacade.java

--- test4/src/java/facades/ParentFacade.java
+++ test4/src/java/facades/ParentFacade.java
@@ -21,7 +21,7 @@ public class ParentFacade extends AbstractFacade<Parent> {
     private EntityManager em;
 
     @Override
-    protected EntityManager getEntityManager() {
+    public EntityManager getEntityManager() {
         return em;
     }
 
