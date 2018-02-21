7a744ffbcdecfa343cce389cc7cc6bc0b246d770
SWE497team <36336916+SWE497team@users.noreply.github.com>
2018-02-21 21:13:13 +0300

    Update UserMB.java

--- test4/src/java/beans/UserMB.java
+++ test4/src/java/beans/UserMB.java
@@ -5,18 +5,38 @@
  */
 package beans;
 
+import entity.AcademicUser;
+import entity.Admin;
+import entity.Instructor;
+import entity.Parent;
+import entity.Student;
 import entity.User;
+import facades.AcademicUserFacade;
+import facades.AdminFacade;
+import facades.InstructorFacade;
+import facades.ParentFacade;
+import facades.StudentFacade;
 
 import facades.UserFacade;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
+import javax.servlet.http.HttpSession;
+
 
 /**
  *
@@ -26,11 +46,72 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class UserMB {
 
-    
+
     private User user;
+    private Admin admin;
+    private Student student;
+    private Instructor instructor;
+    private Parent parent;
+    private AcademicUser academicUser;
+    
     @EJB
     private UserFacade userFacade;
+    @EJB
+    private AdminFacade adminFacade;
+    @EJB
+    private StudentFacade studentFacade;
+    @EJB
+    private InstructorFacade instructorFacade;
+    @EJB
+    private ParentFacade parentFacade;
+    @EJB
+    private AcademicUserFacade academicUserFacade;
+    
+    
     private boolean hasError = false;
+
+    public Parent getParent() {
+        return parent;
+    }
+
+    public void setParent(Parent parent) {
+        this.parent = parent;
+    }
+
+    public AcademicUser getAcademicUser() {
+        return academicUser;
+    }
+
+    public void setAcademicUser(AcademicUser academicUser) {
+        this.academicUser = academicUser;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+    
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+    
+  
     
     public boolean hasError(){
         return this.hasError;
@@ -52,18 +133,26 @@ public class UserMB {
     public UserMB() {
         
         user = new User();
+        admin = new Admin();
+        student = new Student();
+        instructor = new Instructor();
+        parent = new Parent();
+        academicUser = new AcademicUser();
     }
+    
+  
     
      
         
         public String createUser() throws SQLException {
-         
-        Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/swe496", "root","1122qqwwaass");
-        Statement ps = con.createStatement();
-        
-        ps.execute("SELECT * FROM user WHERE username='" + user.getUsername() + "'");
-        ResultSet result = ps.getResultSet();
-        if(result.next()){
+            int id;
+            EntityManager em = userFacade.getEntityManager();
+            Query q = em.createNamedQuery("User.findByUsername");
+
+    
+   
+       
+        if(!q.setParameter("username",user.getUsername()).getResultList().isEmpty()){
             this.hasError = true;
             return "signup1";
         }
@@ -71,25 +160,30 @@ public class UserMB {
                         java.sql.Date creation = new java.sql.Date(calendar.getTime().getTime());
                  user.setCreationDate(creation);
                  userFacade.create(user);
-                
+                    id = user.getId();
+                    
+                   
 
-
-                
-
-        if(user.getUserType().equals("admin"))
-        ps.executeUpdate("INSERT INTO Admin (user)  VALUE ('"+user.getId()+"')");
+        if(user.getUserType().equals("admin")){
+            admin.setUser(id);
+            adminFacade.create(admin);
+        }
+        else if(user.getUserType().equals("parent")){
+            parent.setUser(id);
+            parentFacade.create(parent);
         
-        else if(user.getUserType().equals("parent"))
-        ps.executeUpdate("INSERT INTO Parent (user)  VALUE ('"+user.getId()+"')");
-        
-
+        }
         else if(user.getUserType().equals("student")){
-        ps.executeUpdate("INSERT INTO AcademicUser (user)  VALUE ('"+user.getId()+"')");
-        ps.executeUpdate("INSERT INTO student (user)  VALUE ('"+user.getId()+"')");
+            academicUser.setUser(id);
+            academicUserFacade.create(academicUser);
+            student.setUser(id);
+            studentFacade.create(student);
         }
         else if(user.getUserType().equals("instructor")){
-        ps.executeUpdate("INSERT INTO AcademicUser (user)  VALUE ('"+user.getId()+"')");
-        ps.executeUpdate("INSERT INTO instructor (user)  VALUE ('"+user.getId()+"')");
+            academicUser.setUser(id);
+            academicUserFacade.create(academicUser);
+            instructor.setUser(id);
+            instructorFacade.create(instructor);
         }
   
         
@@ -97,30 +191,76 @@ public class UserMB {
     }
 
 
+  public String logout() {
+		HttpSession session = SessionUtils.getSession();
+		session.invalidate();
+		return "login.xhtml?faces-redirect=true";
+	}
+  
+    public List<User> getUserList() {
+            List<User> list = new ArrayList<User>();
+
+                list =  userFacade.findAll();
+                return list;
+    }
+    
+    
+   
         
-   public String CheckValidUser(){
-       try {
-
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/swe496", "root","1122qqwwaass");
-            Statement stmt=con.createStatement();
-            ResultSet rs =stmt.executeQuery("select * from user where username='"+user.getUsername()+"' and password='"+user.getPassword()+"'");
-            if(rs.next())
-   
-             return "adminPage.xhtml?faces-redirect=true";
-
-         else 
-             return "signup1.xhtml?faces-redirect=true";
-       }
+        public String login (){
+            HttpSession session = null;
+            EntityManager em = userFacade.getEntityManager();
+            Query q =  em.createNamedQuery("User.loginadmin");
             
-        catch(Exception e) {
-            }
-       return "fail";
-      }   
-   
-   
-   
-   
-   
+       
+        if(!q.setParameter("username",user.getUsername()).setParameter("password", user.getPassword()).getResultList().isEmpty()){
+                   session = SessionUtils.getSession();
+		   session.setAttribute("username", user.getUsername()); 
+           
+                   return"adminPage.xhtml?faces-redirect=true";}
+        
+        else { 
+             EntityManager em2 = userFacade.getEntityManager();
+             Query q2 =  em2.createNamedQuery("User.loginstudent");
+             if(!q2.setParameter("username",user.getUsername()).setParameter("password", user.getPassword()).getResultList().isEmpty()){
+                   session = SessionUtils.getSession();
+		   session.setAttribute("username", user.getUsername()); 
+                
+                   return"studentPage.xhtml?faces-redirect=true";}
+             else{
+                 EntityManager em3 =  userFacade.getEntityManager();
+                 Query q3 =  em3.createNamedQuery("User.logininstructor");
+                 if(!q3.setParameter("username",user.getUsername()).setParameter("password", user.getPassword()).getResultList().isEmpty()){
+                   session = SessionUtils.getSession();
+		   session.setAttribute("username", user.getUsername()); 
+                   
+                  return"instructorPage.xhtml?faces-redirect=true";}
+             else  {
+                     
+                 EntityManager em4 =  userFacade.getEntityManager();
+                 Query q4 =  em4.createNamedQuery("User.loginparent");
+                 if(!q4.setParameter("username",user.getUsername()).setParameter("password", user.getPassword()).getResultList().isEmpty()){
+                   session = SessionUtils.getSession();
+		   session.setAttribute("username", user.getUsername()); 
+                  return"parentPage.xhtml?faces-redirect=true";}
+                     
+                 }
+             }
+                 
+        
+        
+        
+        
+        
+        }
+        
+        
+
+             
+        
+        
+          
+            return "login.xhtml?faces-redirect=true";
+                }
 
 }
